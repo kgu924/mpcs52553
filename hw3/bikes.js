67f944ee45f94cb60117ee4af15b8eccb8331b3e
kgunaydin <kaan.gunaydin@gmail.com>
2020-02-09 23:55:54 -0600

    1-2 done

---
+++ hw3/bikes.js
@@ -0,0 +1,46 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+	for (heart of document.querySelectorAll(".fave")) {
+		heart.addEventListener("click", add_to_favorite)
+	}
+})
+
+
+function add_to_favorite() {
+	// console.log(this.innerText);
+	// console.log(this.innerHTML);
+	// console.log(this.textContent);
+	let fav_body = document.querySelector("#favorites tbody")
+
+	if (this.innerText == "♡") {
+		this.innerHTML = "&#9829;"
+
+		let item_row = document.createElement("tr")
+	  	let item_element = document.createElement("td")
+		let text = document.createTextNode(this.parentElement.nextSibling.innerText)
+
+	  	item_element.appendChild(text)
+	  	item_row.appendChild(item_element)
+	  	fav_body.appendChild(item_row)
+	  }
+	else {
+		// console.log("aaaa")
+		this.innerHTML = "&#9825;"
+
+
+		for (elem of fav_body.querySelectorAll("tr")) {
+			if (elem.firstElementChild.innerText ==  this.parentElement.nextSibling.innerText) {
+				elem.remove()
+			}
+		}
+	}
+
+
+
+
+
+
+}
+
+
---
+++ hw3/bikrd.js
@@ -0,0 +1,14 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector(".fave").addEventListener("click", add_to_favorite)
+})
+
+
+function add_to_favorite() {
+	console.log(this.className)
+	
+}
+
+
+document.querySelector()
