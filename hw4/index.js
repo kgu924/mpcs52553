70c34825e30edf2b3aef4de6c7a2e662afa9e174
kgunaydin <kaan.gunaydin@gmail.com>
2020-02-27 00:00:02 -0600

    some hw4 files

--- hw4/index.js
+++ hw4/index.js
@@ -1,25 +1,75 @@
 
 
-function add_element(name) {
+function add_element(name,distance) {
     const wall = document.querySelector("#wall");
     let item = document.createElement("div")
     item.classList.add("col-3")
     item.classList.add("text-center")
 
-    let p_elem = document.createElement("p")
-    let text_elem = document.createTextNode(name)
+    // let p_elem = document.createElement("p")
+    // let text_elem = document.createTextNode("Name: " + name)
+    // let p_elem2 = document.createElement("p")
+    // let text_elem2 = document.createTextNode("Distance: " + distance)
 
-    p_elem.appendChild(text_elem)
-    item.appendChild(p_elem)
+
+    // p_elem.appendChild(text_elem)
+    // p_elem2.appendChild(text_elem2)
+
+    // item.appendChild(p_elem)
+    // item.appendChild(p_elem2)
+    // wall.appendChild(item)
+
+    let card = document.createElement("div")
+    card.classList.add("card")
+    let cardbody = document.createElement("div")
+    cardbody.classList.add("card-body")
+    let cardtitle = document.createElement("h5")
+    cardtitle.classList.add("card-title")
+    let cardtext = document.createElement("p")
+    cardtext.classList.add("card-text")
+    // let cardlink = document.createElement("a")
+    // cardlink.classList.add("card-link")
+
+    let text_elem = document.createTextNode(name)
+    let text_elem2 = document.createTextNode("Distance: " + distance)
+
+    cardtitle.appendChild(text_elem)
+    cardtext.appendChild(text_elem2)
+
+    cardbody.appendChild(cardtitle)
+    cardbody.appendChild(cardtext)
+
+    card.appendChild(cardbody)
+
+    item.appendChild(card)
     wall.appendChild(item)
 
+
+
+
+
+
+
+
+
 }
 
-function sort_by_distance_to_earth(){
-  var items = Object.keys(dict).map(function(key) {
-  return [key, dict[key]];
-});
-}
+// function sort(items){
+//   items.sort(function(first, second) {
+//   return second[1] - first[1];
+//   });
+// }
+
+function create_name_distance_array(all_asteroids){
+  let myarray=[]
+  for (const [key, value] of Object.entries(all_asteroids)) {
+    for (asteroid of value) {
+      myarray.push([asteroid.name,asteroid.close_approach_data[0].miss_distance.astronomical])
+
+    }
+  }
+  return myarray;
+  }
 
 async function getData() {
   console.log("hi")
@@ -31,13 +81,26 @@ async function getData() {
   let http_response = await fetch(apistring)
   let asteroid_data = await http_response.json()
 
+  let name_distance_array = create_name_distance_array(asteroid_data.near_earth_objects)
+  name_distance_array.sort(function(first, second) {
+  return first[1] - second[1];
+  });
 
-  for (const [key, value] of Object.entries(asteroid_data.near_earth_objects)) {
-    for (asteroid of value) {
-      add_element(asteroid.name)
 
-    };
+
+  // console.log(name_distance_array)
+
+  for (i of name_distance_array) {
+    add_element(i[0],i[1])
   }
+
+
+  // for (const [key, value] of Object.entries(asteroid_data.near_earth_objects)) {
+  //   for (asteroid of value) {
+  //     add_element(asteroid.name)
+
+  //   };
+  // }
 
 
   // const ul = document.querySelector("ul");
@@ -60,11 +123,19 @@ async function getData() {
   // }
 }
 
-
-getData()
-
 async function getPic() {
   let myapikey = "MjvmelTq2o2koysV6ovrEeC9WEqifXgrVOrnXd3w"
   let apistring =`https://api.nasa.gov/planetary/apod?api_key=${myapikey}`
 
+  let http_response = await fetch(apistring)
+  let pic_data = await http_response.json()
+
+  let piccard = document.querySelector("#pic div img");
+  piccard.src = pic_data.hdurl
+
+
+
 }
+
+getData()
+getPic()
